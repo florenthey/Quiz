6b17f21a1a764967e18cb2ad601785875efd61ae
Flo <florentheyraud.pro@gmail.com>
2018-04-08 23:11:14 +0200

    v1

---
+++ quizz.js
@@ -0,0 +1,570 @@
+
+/*  question1  */
+document.getElementById('valid').onclick =
+
+  function check(){
+
+    var question1  = document.getElementById('playersfield').value;
+    
+
+    if(question1 != 'Darth Vader'){
+   
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield').style.background = '#FF4500';
+       document.getElementById('result').innerHTML = truefalse;
+       document.getElementById('result').style.visibility = "visible";
+       var question1 = $("playersfield").click(function () {
+                var text = "";
+                $(this).val(text);
+            });
+    }
+
+    if(question1 == 'Darth Vader'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield').style.background = '#7FFF00';
+      document.getElementById('result').innerHTML = truefalse;
+      document.getElementById('solution').style.visibility = "visible";
+    
+    }
+
+    }
+    
+
+/*  question1 end  */
+
+
+
+
+/*  question2  */
+document.getElementById('valid2').onclick =
+
+  function check(){
+
+    var question2  = document.getElementById('playersfield2').value;
+
+
+    if(question2 == 'quatre'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield2').style.background = '#7FFF00';
+      document.getElementById('result2').innerHTML = truefalse;
+      document.getElementById('solution2').style.visibility = "visible";
+    }
+
+    if(question2 != 'quatre'){
+
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield2').style.background = '#FF4500';
+       document.getElementById('result2').innerHTML = truefalse;
+       document.getElementById('result2').style.visibility = "visible";
+    }
+}
+/*  question2 end  */
+
+
+
+/*  question3  */
+document.getElementById('valid3').onclick =
+
+  function check(){
+
+    var question3  = document.getElementById('playersfield3').value;
+
+
+    if(question3 == 'Kakarot'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield3').style.background = '#7FFF00';
+      document.getElementById('result3').innerHTML = truefalse;
+      document.getElementById('solution3').style.visibility = "visible";
+    }
+
+    if(question3 != 'Kakarot'){
+
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield3').style.background = '#FF4500';
+       document.getElementById('result3').innerHTML = truefalse;
+       document.getElementById('result3').style.visibility = "visible";
+    }
+}
+/*  question3 end  */
+
+
+/*  question4  */
+document.getElementById('valid4').onclick =
+
+  function check(){
+
+    var question4  = document.getElementById('playersfield4').value;
+
+
+    if(question4 == 'Mercure'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield4').style.background = '#7FFF00';
+      document.getElementById('result4').innerHTML = truefalse;
+      document.getElementById('solution4').style.visibility = "visible";
+    }
+
+    if(question4 != 'Mercure'){
+
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield4').style.background = '#FF4500';
+       document.getElementById('result4').innerHTML = truefalse;
+       document.getElementById('result4').style.visibility = "visible";
+    }
+}
+/*  question4 end  */
+
+
+/*  question5  */
+document.getElementById('valid5').onclick =
+
+  function check(){
+
+    var question5  = document.getElementById('playersfield5').value;
+
+
+    if(question5 == 'Tyson'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield5').style.background = '#7FFF00';
+      document.getElementById('result5').innerHTML = truefalse;
+      document.getElementById('solution5').style.visibility = "visible";
+    }
+
+    if(question5 != 'Tyson'){
+
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield5').style.background = '#FF4500';
+       document.getElementById('result5').innerHTML = truefalse;
+       document.getElementById('result5').style.visibility = "visible";
+    }
+}
+/*  question5 end  */
+
+
+
+
+/*  question6  */
+document.getElementById('valid6').onclick =
+
+  function check(){
+
+    var question6  = document.getElementById('playersfield6').value;
+
+
+    if(question6 == 'blue meth'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield6').style.background = '#7FFF00';
+      document.getElementById('result6').innerHTML = truefalse;
+      document.getElementById('solution6').style.visibility = "visible";
+    }
+
+    if(question6 != 'blue meth'){
+
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield6').style.background = '#FF4500';
+       document.getElementById('result6').innerHTML = truefalse;
+       document.getElementById('result6').style.visibility = "visible";
+    }
+}
+/*  question6 end  */
+
+
+
+/*  question7  */
+document.getElementById('valid7').onclick =
+
+  function check(){
+
+    var question7  = document.getElementById('playersfield7').value;
+
+
+    if(question7 == 'Tekken'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield7').style.background = '#7FFF00';
+      document.getElementById('result7').innerHTML = truefalse;
+      document.getElementById('solution7').style.visibility = "visible";
+    }
+
+    if(question7 != 'Tekken'){
+
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield7').style.background = '#FF4500';
+       document.getElementById('result7').innerHTML = truefalse;
+       document.getElementById('result7').style.visibility = "visible";
+    }
+}
+/*  question7 end  */
+
+
+
+
+/*  question8  */
+document.getElementById('valid8').onclick =
+
+  function check(){
+
+    var question8  = document.getElementById('playersfield8').value;
+
+
+    if(question8 == 'chakra'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield8').style.background = '#7FFF00';
+      document.getElementById('result8').innerHTML = truefalse;
+      document.getElementById('solution8').style.visibility = "visible";
+    }
+
+    if(question8 != 'chakra'){
+
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield8').style.background = '#FF4500';
+       document.getElementById('result8').innerHTML = truefalse;
+       document.getElementById('result8').style.visibility = "visible";
+    }
+}
+/*  question8 end  */
+
+
+
+
+
+/*  question9  */
+document.getElementById('valid9').onclick =
+
+  function check(){
+
+    var question9  = document.getElementById('playersfield9').value;
+
+
+    if(question9 == 'Tamzarian'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield9').style.background = '#7FFF00';
+      document.getElementById('result9').innerHTML = truefalse;
+      document.getElementById('solution9').style.visibility = "visible";
+    }
+
+    if(question9 != 'Tamzarian'){
+
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield9').style.background = '#FF4500';
+       document.getElementById('result9').innerHTML = truefalse;
+       document.getElementById('result9').style.visibility = "visible";
+    }
+}
+/*  question9 end  */
+
+
+
+
+/*  question10  */
+document.getElementById('valid10').onclick =
+
+  function check(){
+
+    var question10  = document.getElementById('playersfield10').value;
+
+
+    if(question10 == 'Servietsky'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield10').style.background = '#7FFF00';
+      document.getElementById('result10').innerHTML = truefalse;
+      document.getElementById('solution10').style.visibility = "visible";
+    }
+
+    if(question10 != 'Servietsky'){
+
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield10').style.background = '#FF4500';
+       document.getElementById('result10').innerHTML = truefalse;
+       document.getElementById('result10').style.visibility = "visible";
+    }
+}
+/*  question10 end  */
+
+
+
+/*  question11  */
+document.getElementById('valid11').onclick =
+
+  function check(){
+
+    var question11  = document.getElementById('playersfield11').value;
+
+
+    if(question11 == 'Dagobah'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield11').style.background = '#7FFF00';
+      document.getElementById('result11').innerHTML = truefalse;
+      document.getElementById('solution11').style.visibility = "visible";
+    }
+
+    if(question11 != 'Dagobah'){
+
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield11').style.background = '#FF4500';
+       document.getElementById('result11').innerHTML = truefalse;
+       document.getElementById('result11').style.visibility = "visible";
+    }
+}
+/*  question11 end  */
+
+
+
+/*  question12  */
+document.getElementById('valid12').onclick =
+
+  function check(){
+
+    var question12  = document.getElementById('playersfield12').value;
+
+
+    if(question12 == 'Gally'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield12').style.background = '#7FFF00';
+      document.getElementById('result12').innerHTML = truefalse;
+      document.getElementById('solution12').style.visibility = "visible";
+    }
+
+    if(question12 != 'Gally'){
+
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield12').style.background = '#FF4500';
+       document.getElementById('result12').innerHTML = truefalse;
+       document.getElementById('result12').style.visibility = "visible";
+    }
+}
+/*  question12 end  */
+
+
+
+/*  question13  */
+document.getElementById('valid13').onclick =
+
+  function check(){
+
+    var question13  = document.getElementById('playersfield13').value;
+
+
+    if(question13 == 'quatorze'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield13').style.background = '#7FFF00';
+      document.getElementById('result13').innerHTML = truefalse;
+      document.getElementById('solution13').style.visibility = "visible";
+    }
+
+    if(question13 != 'quatorze'){
+
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield13').style.background = '#FF4500';
+       document.getElementById('result13').innerHTML = truefalse;
+       document.getElementById('result13').style.visibility = "visible";
+    }
+}
+/*  question13 end  */
+
+
+
+
+/*  question14  */
+document.getElementById('valid14').onclick =
+
+  function check(){
+
+    var question14  = document.getElementById('playersfield14').value;
+
+
+    if(question14 == 'Roland'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield14').style.background = '#7FFF00';
+      document.getElementById('result14').innerHTML = truefalse;
+      document.getElementById('solution14').style.visibility = "visible";
+    }
+
+    if(question14 != 'Roland'){
+
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield14').style.background = '#FF4500';
+       document.getElementById('result14').innerHTML = truefalse;
+       document.getElementById('result14').style.visibility = "visible";
+    }
+}
+/*  question14 end  */
+
+
+
+
+/*  question15  */
+document.getElementById('valid15').onclick =
+
+  function check(){
+
+    var question15  = document.getElementById('playersfield15').value;
+
+
+    if(question15 == 'Medievil'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield15').style.background = '#7FFF00';
+      document.getElementById('result15').innerHTML = truefalse;
+      document.getElementById('solution15').style.visibility = "visible";
+    }
+
+    if(question15 != 'Medievil'){
+
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield15').style.background = '#FF4500';
+       document.getElementById('result15').innerHTML = truefalse;
+       document.getElementById('result15').style.visibility = "visible";
+    }
+}
+/*  question15 end  */
+
+
+/*  question16  */
+document.getElementById('valid16').onclick =
+
+  function check(){
+
+    var question16  = document.getElementById('playersfield16').value;
+
+
+    if(question16 == 'Le château dans le ciel'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield16').style.background = '#7FFF00';
+      document.getElementById('result16').innerHTML = truefalse;
+      document.getElementById('solution16').style.visibility = "visible";
+    }
+
+    if(question16 != 'Le château dans le ciel'){
+
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield16').style.background = '#FF4500';
+       document.getElementById('result16').innerHTML = truefalse;
+       document.getElementById('result16').style.visibility = "visible";
+    }
+}
+/*  question16 end  */
+
+
+
+/*  question17  */
+document.getElementById('valid17').onclick =
+
+  function check(){
+
+    var question17  = document.getElementById('playersfield17').value;
+
+
+    if(question17 == 'diplodocus'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield17').style.background = '#7FFF00';
+      document.getElementById('result17').innerHTML = truefalse;
+      document.getElementById('solution17').style.visibility = "visible";
+    }
+
+    if(question17 != 'diplodocus'){
+
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield17').style.background = '#FF4500';
+       document.getElementById('result17').innerHTML = truefalse;
+       document.getElementById('result17').style.visibility = "visible";
+    }
+}
+/*  question17 end  */
+
+
+
+/*  question18  */
+document.getElementById('valid18').onclick =
+
+  function check(){
+
+    var question18  = document.getElementById('playersfield18').value;
+
+
+    if(question18 == 'Emmet'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield18').style.background = '#7FFF00';
+      document.getElementById('result18').innerHTML = truefalse;
+      document.getElementById('solution18').style.visibility = "visible";
+    }
+
+    if(question18 != 'Emmet'){
+
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield18').style.background = '#FF4500';
+       document.getElementById('result18').innerHTML = truefalse;
+       document.getElementById('result18').style.visibility = "visible";
+    }
+}
+/*  question18 end  */
+
+
+/*  question19 */
+document.getElementById('valid19').onclick =
+
+  function check(){
+
+    var question19  = document.getElementById('playersfield19').value;
+
+
+    if(question19 == 'Miles Morales'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield19').style.background = '#7FFF00';
+      document.getElementById('result19').innerHTML = truefalse;
+      document.getElementById('solution19').style.visibility = "visible";
+    }
+
+    if(question19 != 'Miles Morales'){
+
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield19').style.background = '#FF4500';
+       document.getElementById('result19').innerHTML = truefalse;
+       document.getElementById('result19').style.visibility = "visible";
+    }
+}
+/*  question19 end  */
+
+
+
+/*  question20 */
+document.getElementById('valid20').onclick =
+
+  function check(){
+
+    var question20  = document.getElementById('playersfield20').value;
+
+
+    if(question20 == 'faux'){
+
+      var truefalse = 'Correct!';
+      document.getElementById('playersfield20').style.background = '#7FFF00';
+      document.getElementById('result20').innerHTML = truefalse;
+      document.getElementById('solution20').style.visibility = "visible";
+    }
+
+    if(question20 != 'faux'){
+
+       var truefalse = 'Faux!';
+       document.getElementById('playersfield20').style.background = '#FF4500';
+       document.getElementById('result20').innerHTML = truefalse;
+       document.getElementById('result20').style.visibility = "visible";
+    }
+}
+/*  question20 end  */
